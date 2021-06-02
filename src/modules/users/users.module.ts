8d08de03c1a55de1095cdb14ae57a6361620cae0
nettorodrigues <neto6391@gmail.com>
2021-06-02 11:01:02 -0300

    changes in module User for import Model and repository

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { usersProviders } from './users.providers';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  providers: [UsersService, ...usersProviders]
+})
 export class UsersModule {}
