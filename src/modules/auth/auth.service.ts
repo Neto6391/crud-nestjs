805398330ffddfc639b7a9fb8e6ca615e71cbf28
nettorodrigues <neto6391@gmail.com>
2021-06-02 23:25:15 -0300

    changes in auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
 
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UsersService) { }
+  
+    constructor(
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService
+    ) { }
 
     async validateUser(username: string, pass: string) {
         const user = await this.userService.findOneByEmail(username);
@@ -21,6 +26,33 @@ export class AuthService {
 
         const { password, ...result } = user['dataValues'];
         return result;
+    }
+
+    public async login(user) {
+        const token = await this.generateToken(user);
+        return { user, token };
+    }
+
+    public async create(user) {
+        const pass = await this.hashPassword(user.password);
+
+        const newUser = await this.userService.create({ ...user, password: pass });
+        const { password, ...result } = newUser['dataValues'];
+        
+        const token = await this.generateToken(result);
+
+        return { user: result, token };
+        
+    }
+
+    private async generateToken(user) {
+        const token = await this.jwtService.signAsync(user);
+        return token;
+    }
+
+    private async hashPassword(password) {
+        const hash = await bcrypt.hash(password, 10);
+        return hash;
     }
 
     private async comparePassword(enteredPassword, dbPassword) {
